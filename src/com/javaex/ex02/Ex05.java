d7430ba6fe1ef7865640e29d1b0c1351a0d6da72
'Ryu-Yean' <'uyti0713@gmail.com'>
2020-11-11 16:20:16 +0900

    커미터 변경

--- src/com/javaex/ex02/Ex05.java
+++ src/com/javaex/ex02/Ex05.java
@@ -20,6 +20,7 @@ public class Ex05 {
 		System.out.println("당신의 이름은 " + name +"나이는 " + age+ "입니다.");
 		
 		System.out.println("깃허브 테스트 문장");
+		System.out.println("깃허브 테스트 문장");
 		
 		sc.close();
 	}
