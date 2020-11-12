2414ca4b5f254e42c95ba3e7b04373cf770f3784
'Ryu-Yena' <'uyti0713@gmail.com'>
2020-11-12 12:03:27 +0900

    if문 예제

---
+++ src/com/javaex/ex03/Ex03.java
@@ -0,0 +1,26 @@
+package com.javaex.ex03;
+
+import java.util.Scanner;
+
+public class Ex03 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("숫자를 입력하시오.");
+		System.out.println("숫자 : ");
+		
+		int point = sc.nextInt();
+		
+		if(point>0) {
+			
+			System.out.println("양수");
+		}else if(point<0) {
+			System.out.println("음수");
+		}
+		
+		sc.close();
+
+	}
+
+}
