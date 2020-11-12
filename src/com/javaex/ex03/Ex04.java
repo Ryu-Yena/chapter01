1f599478979a3fa3e14635444ed59ffd05745fe3
'Ryu-Yena' <'uyti0713@gmail.com'>
2020-11-12 13:14:38 +0900

    야간 근무시간 1.5배

--- src/com/javaex/ex03/Ex04.java
+++ src/com/javaex/ex03/Ex04.java
@@ -10,15 +10,17 @@ public class Ex04 {
 		System.out.print("근무시간 : ");
 		int time = sc.nextInt();
 		int money, overtime;
+		int unit = 10000;
 		
 		if(time<=8){
-			money = time*10000;
+			money = time*unit;
 			System.out.println("임금은 "+money+"원 입니다.");
 		}else if(time>8) {
 			
 			overtime = time - 8;
 			
-			money = (time-overtime)*10000 + (overtime*12000);
+			//money = (time-overtime)*unit + (overtime*12000);
+			money = (int)((time-overtime)*unit + overtime*(unit*1.5));
 			System.out.println("임금은 "+ money +"원 입니다.");
 		}
 		sc.close();
