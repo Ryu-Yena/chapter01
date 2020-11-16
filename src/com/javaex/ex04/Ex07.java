e6bcc601b3456704449d43b0304875e27f365324
'Ryu-Yena' <'uyti0713@gmail.com'>
2020-11-16 14:36:11 +0900

    다중 for문 (****** < 6줄 찍기)

---
+++ src/com/javaex/ex04/Ex07.java
@@ -0,0 +1,15 @@
+package com.javaex.ex04;
+
+public class Ex07 {
+
+	public static void main(String[] args) {
+		int i,j;
+		
+		for(i=1;i<=6;i++) {
+			for(j=1;j<=6;j++) {
+				System.out.print("*");
+			}System.out.println("\n");
+		}
+	}
+
+}
