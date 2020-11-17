617dc6168b1c561d70f58e4d5d4cf104fa6078f2
'Ryu-Yena' <'uyti0713@gmail.com'>
2020-11-17 11:31:13 +0900

    반복문 연습문제 (숫자를 입력받아 3의 배수인지 판별하는 프로그램)

--- src/com/javaex/ex04/Ex09.java
+++ src/com/javaex/ex04/Ex09.java
@@ -4,6 +4,7 @@ public class Ex09 {
 
 	public static void main(String[] args) {
 		int i=1;
+		int j;
 		
 		while(true) {
 			if(i%6==0 && i%14==0) {
@@ -11,6 +12,15 @@ public class Ex09 {
 				break;
 			}
 			i++;
+		}
+		
+		
+		//예제 2
+		for(j=1; j<=100;j++) {
+			if(j%6==0 && j%14==0) {
+				System.out.println(i);
+				break;
+			}
 		}
 
 	}
